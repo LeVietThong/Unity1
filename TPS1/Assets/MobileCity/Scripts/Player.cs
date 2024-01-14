499e26109d8f8591634699aae65bd070a01eda20
LeVietThong <levietthong92@gmail.com>
2024-01-14 23:10:24 +0700

    TPS1

---
+++ TPS1/Assets/MobileCity/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem confettiBlastRainbow; // Kéo và thả particle system vào đây từ tab Project
+    public float particleCooldownTime = 5f;
+    private float particleCooldownTimer;
+    private bool canShootParticle = true;
+
+    private void Update()
+    {
+        if (!canShootParticle)
+        {
+            particleCooldownTimer -= Time.deltaTime;
+            if (particleCooldownTimer <= 0)
+            {
+                canShootParticle = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && canShootParticle)
+        {
+            confettiBlastRainbow.Play();
+            canShootParticle = false;
+            particleCooldownTimer = particleCooldownTime;
+            Debug.Log("123");
+        }
+    }
+}
---
+++ TPS1/Assets/MobileCity/Scripts/SplashController.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CanvasGroup canvasGroup;
+    public string nextSceneName;
+    public float fadeDuration = 1f;
+    public float delayBeforeFadeOut = 1f;
+
+    private void Start()
+    {
+        // Đặt alpha của CanvasGroup là 0
+        canvasGroup.alpha = 0;
+
+        // Sử dụng DOTween để tạo hiệu ứng fade in
+        canvasGroup.DOFade(1f, fadeDuration)
+            .OnComplete(() =>
+            {
+                // Gọi hàm để delay trước khi fade out
+                Invoke(nameof(FadeOut), delayBeforeFadeOut);
+            });
+    }
+
+    private void FadeOut()
+    {
+        // Sử dụng DOTween để tạo hiệu ứng fade out
+        canvasGroup.DOFade(0f, fadeDuration)
+            .OnComplete(() =>
+            {
+                // Load scene tiếp theo sau khi fade out hoàn thành
+                SceneManager.LoadScene(nextSceneName);
+            });
+    }
+}
